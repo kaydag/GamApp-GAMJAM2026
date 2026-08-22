1367cf924d75ee7149f0cc71889818ed141d45f5
kaydag <dangmaikhanhkn@gmail.com>
2026-08-23 04:42:11 +0700

    feat: create UI for intro scene

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -44,7 +44,6 @@ public class PlayerHealth : MonoBehaviour
     void TakeDamage(float damage)
     {
         currentHealth -= damage;
-        Debug.Log(currentHealth);
         GameEvent.HealthChanged?.Invoke(gameObject,currentHealth, maxHealth);
         if (currentHealth <= 0)
         {
